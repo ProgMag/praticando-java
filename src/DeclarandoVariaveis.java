32f5c5dff5417dcf6458b94fd4c94d58fa8927e8
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-27 17:59:10 -0300

    Adiciona primeira atividade

---
+++ src/DeclarandoVariaveis.java
@@ -0,0 +1,15 @@
+public class DeclarandoVariaveis {
+    public static void main(String[] args) {
+       String nome = "Guilherme";
+       int idade = 22;
+       double altura = 1.78;
+       boolean estudante = true;
+
+        System.out.format("""
+                Aluno: %s
+                Idade: %d
+                Altura: %.2f
+                Estudante: %s%n
+                """, nome, idade, altura, estudante);
+    }
+}

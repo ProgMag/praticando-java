b75c3a64d66044a72c57713b37ad12ef9d922647
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-27 18:30:11 -0300

    Adiciona atividade quatro

---
+++ src/alura/praticando/variaveis/atividadequatro/ConversaoTemperatura.java
@@ -0,0 +1,21 @@
+package alura.praticando.variaveis.atividadequatro;
+
+import java.util.Scanner;
+
+public class ConversaoTemperatura {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a temperatura em grau Celsius: ");
+
+        int grauCelsius = sc.nextInt();
+
+        double fahrenheit = calcularTemperaturaFahrenheit(grauCelsius);
+
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
+    }
+
+    public static double calcularTemperaturaFahrenheit(double temperaturaCelsius) {
+            return (temperaturaCelsius * (9 / 5)) + 32;
+    }
+}

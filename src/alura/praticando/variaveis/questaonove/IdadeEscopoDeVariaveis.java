7c86a84f999944164eae0957c54a94bc6484e077
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-28 12:10:58 -0300

    Adiciona atividade nove

--- src/alura/praticando/variaveis/questaonove/IdadeEscopoDeVariaveis.java
+++ src/alura/praticando/variaveis/questaonove/IdadeEscopoDeVariaveis.java
@@ -3,5 +3,16 @@ package alura.praticando.variaveis.questaonove;
 public class IdadeEscopoDeVariaveis {
     public static void main(String[] args) {
 
+        int idade = 10;
+        String mensagem;
+
+        if (idade >= 18) {
+            mensagem = ("Você é maior de idade.");
+        } else {
+            mensagem = ("Você é menor de idade.");
+        }
+
+        System.out.println(mensagem);
+
     }
 }

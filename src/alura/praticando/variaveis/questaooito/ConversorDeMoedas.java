ad041556d6677c9c3aeeef1733d99825290fd3c8
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-28 12:03:35 -0300

    Adiciona atividade oito

--- src/alura/praticando/variaveis/questaooito/ConversorDeMoedas.java
+++ src/alura/praticando/variaveis/questaooito/ConversorDeMoedas.java
@@ -1,4 +1,18 @@
 package alura.praticando.variaveis.questaooito;
 
+import java.util.Scanner;
+
 public class ConversorDeMoedas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double cotacaoAtual = 5.06d;
+
+        double valorEmReal = input.nextDouble();
+
+        double valorEmDola = valorEmReal / cotacaoAtual;
+
+        System.out.format("""
+                %.2f reias é %.2f dólares
+                """, valorEmReal, valorEmDola);
+    }
 }

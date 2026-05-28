08ca36eb553d75cd837750259975ce987dd98dc5
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-28 14:42:53 -0300

    Adiciona correção ao código

--- src/alura/praticando/java/ifeelse/questaocinco/VerificaCompras.java
+++ src/alura/praticando/java/ifeelse/questaocinco/VerificaCompras.java
@@ -9,18 +9,18 @@ public class VerificaCompras {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Qual o valor da comprar?");
+        System.out.println("Qual o valor da compra?");
         double valor = input.nextDouble();
 
-        double valorComDesconto = calcularDesconto(valor, desconto);
-        verificarCompras(valor, valorComDesconto, desconto);
+        verificarCompras(valor, desconto);
 
     }
 
-    public static void verificarCompras(double valorDaCompra, double valorComDesconto, double desconto) {
+    public static void verificarCompras(double valorDaCompra,double desconto) {
 
         if (valorDaCompra >= 100) {
-            System.out.println("Desconto de " + desconto + "% aplicado");
+            double valorComDesconto = calcularDesconto(valorDaCompra, desconto);
+            System.out.println("Desconto de " + desconto * 100 + "% aplicado");
             System.out.println("Novo valor: R$" + valorComDesconto);
         } else {
             System.out.println("Nenhum desconto aplicado");

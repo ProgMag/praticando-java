4d1412b2b51dd45cf778fd0a47de6c6c4ca6651f
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-28 14:12:47 -0300

    Adiciona atividade quatro

--- src/alura/praticando/java/ifeelse/questaoquatro/CoparaNumero.java
+++ src/alura/praticando/java/ifeelse/questaoquatro/CoparaNumero.java
@@ -1,7 +1,29 @@
 package alura.praticando.java.ifeelse.questaoquatro;
 
+import java.util.Scanner;
+
 public class CoparaNumero {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o primerio numero: ");
+        int primeiroNumero =  input.nextInt();
+
+        System.out.println("Digite o segundo numero: ");
+        int segundoNumero =  input.nextInt();
+
+        verificarNumeros(primeiroNumero, segundoNumero);
+
+    }
+
+    public static void verificarNumeros(int primeiroNumero, int segundoNumero){
+        if (primeiroNumero > segundoNumero) {
+            System.out.println(primeiroNumero + " é maior que " + segundoNumero);
+        } else if (primeiroNumero < segundoNumero) {
+            System.out.println(segundoNumero + " é maior que " + primeiroNumero);
+        } else {
+            System.out.println(primeiroNumero + " é igual a " + segundoNumero);
+        }
     }
 }

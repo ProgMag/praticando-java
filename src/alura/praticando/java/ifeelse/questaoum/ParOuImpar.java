bda10f6d6441bb11a37d92cb71b1f470702ddd4e
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-28 13:36:33 -0300

    Adiciona atividade um

--- src/alura/praticando/java/ifeelse/questaoum/ParOuImpar.java
+++ src/alura/praticando/java/ifeelse/questaoum/ParOuImpar.java
@@ -1,7 +1,24 @@
 package alura.praticando.java.ifeelse.questaoum;
 
+import java.util.Scanner;
+
 public class ParOuImpar {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+
+        int numeroPaOuImpar = input.nextInt();
+
+        verificarParOuImpar(numeroPaOuImpar);
+
+    }
+
+    public static void verificarParOuImpar(int numeroPaOuImpar) {
+        if (numeroPaOuImpar % 2 == 0) {
+            System.out.println(numeroPaOuImpar + " é par");
+        } else {
+            System.out.println(numeroPaOuImpar + " é ímpar");
+        }
     }
 }

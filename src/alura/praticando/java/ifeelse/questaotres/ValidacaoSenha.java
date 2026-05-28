16b3dc32f001dcfe6bae68ebebcc8b551e65c4b3
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-28 14:04:41 -0300

    Adiciona atividade tres

--- src/alura/praticando/java/ifeelse/questaotres/ValidacaoSenha.java
+++ src/alura/praticando/java/ifeelse/questaotres/ValidacaoSenha.java
@@ -1,7 +1,27 @@
 package alura.praticando.java.ifeelse.questaotres;
 
+import java.util.Scanner;
+
 public class ValidacaoSenha {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite a senha:");
+        String minhaSenha = input.nextLine();
+
+        validarSenha(minhaSenha);
+
+    }
+
+    public static void validarSenha(String minhaSenha) {
+
+        String senha = "123456";
+
+        if (minhaSenha.equals(senha)) {
+            System.out.println("Acesso permitido!");
+        } else {
+            System.out.println("Senha invalida");
+        }
     }
 }

2a6431cf76f784e28fb6098481e801c3d5935f84
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-29 10:56:54 -0300

    Adiciona resolução da atvidade dez

--- src/alura/praticando/java/ifeelse/questaodez/VerificacaoCodigo.java
+++ src/alura/praticando/java/ifeelse/questaodez/VerificacaoCodigo.java
@@ -1,7 +1,35 @@
 package alura.praticando.java.ifeelse.questaodez;
 
+import java.util.Scanner;
+
 public class VerificacaoCodigo {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        int codigo = 2023;
+        int nivelPermissaoMinimo = 1;
+        int nivelPermissaoMaximo = 3;
+
+        System.out.println("Digite o código de acesso: ");
+        int codigoUsuario = input.nextInt();
+
+        System.out.println("Digite o nível de permissão: ");
+        int nivelPermissao = input.nextInt();
+
+        boolean codigoCorreto = codigoUsuario == codigo; // true
+        boolean permissaoCorreta = nivelPermissao >= nivelPermissaoMinimo && nivelPermissao <= nivelPermissaoMaximo; // true
+
+        verificarPermissao(codigoCorreto, permissaoCorreta);
+    }
+
+    public static void verificarPermissao(boolean codigo, boolean nivelPermissao) {
+        if (codigo && nivelPermissao) { // true
+            System.out.println("Acesso permitido. Bem-vindo ao sistema!");
+        } else if (!(codigo)) { // false
+            System.out.println("Acesso negado. Senha incorreta");
+        } else {
+            System.out.println("Acesso negado. Nível de permissão errado");
+        }
     }
 }

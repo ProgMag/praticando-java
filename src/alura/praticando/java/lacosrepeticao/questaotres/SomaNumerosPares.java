6e43442c923e7dbc926b47041cc799023bb7e6de
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-29 12:11:41 -0300

    Adiciona resolução da atividade três

--- src/alura/praticando/java/lacosrepeticao/questaotres/SomaNumerosPares.java
+++ src/alura/praticando/java/lacosrepeticao/questaotres/SomaNumerosPares.java
@@ -3,5 +3,20 @@ package alura.praticando.java.lacosrepeticao.questaotres;
 public class SomaNumerosPares {
     public static void main(String[] args) {
 
+        int somarNumerosPares = 0;
+
+        int resultadoSoma = somarPares(somarNumerosPares);
+
+        System.out.println("A soma dos números pares de 1 a 100 é: " + resultadoSoma);
+    }
+
+    static int somarPares(int somarNumerosPares) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                somarNumerosPares += i;
+            }
+        }
+        return somarNumerosPares;
     }
 }

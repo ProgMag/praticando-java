51bbd6ed40413e5407da789fe8589d4e0a15ee59
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-29 12:51:34 -0300

    Adiciona resolução da atividade quatro

--- src/alura/praticando/java/lacosrepeticao/questaoquatro/CalculaFatorial.java
+++ src/alura/praticando/java/lacosrepeticao/questaoquatro/CalculaFatorial.java
@@ -1,7 +1,26 @@
 package alura.praticando.java.lacosrepeticao.questaoquatro;
 
+import java.util.Scanner;
+
 public class CalculaFatorial {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        double numero = input.nextDouble();
+
+        double resultadoFatorial = calcularFatorial(numero);
+
+        System.out.println("O fatorial de " + numero + " é: " + resultadoFatorial);
+    }
+
+    static double calcularFatorial(double numero) {
+        int fatorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        return fatorial;
     }
 }

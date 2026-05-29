3e6e79cb3d8c86064037176cae3b0f9ea41da4cd
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-29 13:16:00 -0300

    Adiciona atividade cinco

--- src/alura/praticando/java/lacosrepeticao/questaocinco/EncontraMaiorNumero.java
+++ src/alura/praticando/java/lacosrepeticao/questaocinco/EncontraMaiorNumero.java
@@ -1,7 +1,29 @@
 package alura.praticando.java.lacosrepeticao.questaocinco;
 
+import java.util.Scanner;
+
 public class EncontraMaiorNumero {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        int[] listaNumeros = new int[5];
+
+        System.out.println("Digite os números separados por espaço");
+        listaNumeros[0] = input.nextInt();
+        listaNumeros[1] = input.nextInt();
+        listaNumeros[2] = input.nextInt();
+        listaNumeros[3] = input.nextInt();
+        listaNumeros[4] = input.nextInt();
+
+        int maiorNumero = listaNumeros[0];
+
+        for (int i = 1; i < listaNumeros.length; i++) {
+            if (listaNumeros[i] > maiorNumero) {
+                maiorNumero = listaNumeros[i];
+            }
+        }
+
+        System.out.println("O maior número é: " + maiorNumero);
     }
 }

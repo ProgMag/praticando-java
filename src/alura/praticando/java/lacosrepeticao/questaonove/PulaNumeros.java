4d0f836c77d597fd14664dd41ba8fa3991732d39
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-30 19:00:53 -0300

    Adiciona resolução da atividade nove

--- src/alura/praticando/java/lacosrepeticao/questaonove/PulaNumeros.java
+++ src/alura/praticando/java/lacosrepeticao/questaonove/PulaNumeros.java
@@ -1,7 +1,25 @@
 package alura.praticando.java.lacosrepeticao.questaonove;
 
+import java.util.Scanner;
+
 public class PulaNumeros {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int numero = input.nextInt();
+
+        imprimirNumeros(numero);
+
+    }
+
+    static void imprimirNumeros(int numero) {
+        for (int i = 1; i <= numero; i++) {
+            if (i % 10 == 5) {
+                continue;
+            }
+            System.out.println(i + " ");
+        }
     }
 }

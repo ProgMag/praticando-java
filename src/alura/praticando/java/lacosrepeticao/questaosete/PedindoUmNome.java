49af6b930a0fd170a4462f4aa6ca4ec1ec258f06
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-29 21:58:18 -0300

    Adiciona resolção da atividade sete

--- src/alura/praticando/java/lacosrepeticao/questaosete/PedindoUmNome.java
+++ src/alura/praticando/java/lacosrepeticao/questaosete/PedindoUmNome.java
@@ -1,7 +1,24 @@
 package alura.praticando.java.lacosrepeticao.questaosete;
 
+import java.util.Scanner;
+
 public class PedindoUmNome {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        String nome;
+
+        do {
+            System.out.println("Digite seu nome: ");
+            nome = input.nextLine();
+
+            if (nome.length() < 3) {
+                System.out.println("Nome inválido. Digite novamente.");
+            }
+        } while (nome.length() < 3);
+
+        System.out.println("Nome \"" + nome + "\" cadastrado com sucesso!");
+        input.close();
     }
 }

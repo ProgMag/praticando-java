4e3e08584dc6ae9dcd09245fef0d73079c61bc68
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-05-30 18:39:55 -0300

    Adiciona resolução da atividade oito

--- src/alura/praticando/java/lacosrepeticao/questaooito/ParOuImpar.java
+++
@@ -1,7 +0,0 @@
-package alura.praticando.java.lacosrepeticao.questaooito;
-
-public class ParOuImpar {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/alura/praticando/java/lacosrepeticao/questaooito/PositivoOuNegativo.java
@@ -0,0 +1,40 @@
+package alura.praticando.java.lacosrepeticao.questaooito;
+
+import java.util.Scanner;
+
+public class PositivoOuNegativo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int totalNegativos = 0;
+        int totalPositivos = 0;
+
+        while (true) {
+
+            System.out.println("Digite um número (ou 'fim' para encerrar): ");
+            String escolha = input.nextLine().trim();
+
+            if (escolha.equalsIgnoreCase("fim")) {
+                break;
+            }
+
+            try {
+                int numeroPositivoNegativo = Integer.parseInt(escolha);
+
+                if (numeroPositivoNegativo < 0) {
+                    totalNegativos++;
+                } else if (numeroPositivoNegativo > 0) {
+                    totalPositivos++;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida");
+            }
+        }
+
+        System.out.println("Números positivos: " + totalPositivos);
+        System.out.println("Números negativos: " + totalNegativos);
+
+        input.close();
+    }
+}
